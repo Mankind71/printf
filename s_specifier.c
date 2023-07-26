46a242b60441e4bab921e9fba7b9df72e750b409
mankind71 <makindetolulope37@gmail.com>
2023-07-26 04:11:06 +0100

    betty fixes

--- s_specifier.c
+++ s_specifier.c
@@ -1,18 +1,17 @@
-
 #include "main.h"
 /**
  * s_specifier - prints variable of type str to sdout
- *  
+ *
  * @args: variable pointer to print
  * Return: number of words written
 */
 
 int s_specifier(va_list args)
 {
-    char *str = va_arg(args, char *);
-    int num_written = 0;
+	char *str = va_arg(args, char *);
+	int num_written = 0;
 
-    while (*str)
-	    num_written += my_write2(str++);
-    return (num_written);
+	while (*str)
+		num_written += my_write2(str++);
+	return (num_written);
 }
